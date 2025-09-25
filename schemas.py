86762ed98ead3a6b290b04bc7343dfd0a29a4ed1
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-09-25 13:21:35 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,1 +1,12 @@
+# schemas.py
+from pydantic import BaseModel
+
+class AccountCreate(BaseModel):
+    owner: str
+    balance: float
+
+class TransactionCreate(BaseModel):
+    account_id: int
+    type: str
+    amount: float
 
